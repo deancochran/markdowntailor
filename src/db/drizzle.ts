4aaf4f1b43f0fb57afc035f887cd173d325ffd57
Dean Cochran <charles.dean.cochran@outlook.com>
2025-06-26 15:59:58 -0400

    update migrations process

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -6,7 +6,6 @@ import { Pool } from "pg";
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
   console.error("DATABASE_URL environment variable is not set");
-  throw new Error("DATABASE_URL is required");
 }
 
 const pool = new Pool({
