49109f0e5b9ca5918f6d3bf629fc49711f8cfa1e
Dean Cochran <charles.dean.cochran@outlook.com>
2025-06-27 19:17:08 -0400

    ssl on migrate

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,5 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
-const db = drizzle(`${process.env.DATABASE_URL}`);
+const db = drizzle(`${process.env.DATABASE_URL}`, {
+  ssl: process.env.NODE_ENV === "production" ? true : undefined,
+});
 
 export { db };
