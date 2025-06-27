709413b16ca214ab32ca947af33886c0b6a45918
Dean Cochran <charles.dean.cochran@outlook.com>
2025-06-27 19:03:40 -0400

    moving migrations into instrumentation

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,11 +2,12 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
+    await import("../sentry.server.config");
+    await import("./db/migrate");
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    await import("./sentry.edge.config");
+    await import("../sentry.edge.config");
   }
 }
 
